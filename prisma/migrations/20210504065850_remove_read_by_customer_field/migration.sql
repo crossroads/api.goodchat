aaf537c78f840b8a2c77f28dd96bb534f40cf13e
Patrick R <patrick.rabier@gmail.com>
2021-05-04 14:59:12 +0800

    :card_file_box: DB: remove readByCustomer field (replaced with ReadReceipts)

---
+++ prisma/migrations/20210504065850_remove_read_by_customer_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `readByCustomer` on the `Conversation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Conversation" DROP COLUMN "readByCustomer";
